9af32fba6f1a14e275c639798435fe17d6e3f5e0
Sarthak Singh <singharmy6789@gmail.com>
2024-05-10 00:46:35 +0530

    Web task

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend developer...", "Web Developer...", "Wordpress Developer..."],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
